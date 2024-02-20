a98976c8f050b9ae55ede44c3c14753fee5d963e
Nicholas Smith <nsmith562630@gmial.com>
2024-02-20 11:56:32 -0600

    Created For Loop for subnet array

--- ARMTemplate/YamlParsingProject/Test.ps1
+++ ARMTemplate/YamlParsingProject/Test.ps1
@@ -19,13 +19,13 @@ $subnets = @()
 For ($i=0; $i -lt $yaml.subnets.Count; $i++) {
     [array]$subnets += @{name = $yaml.subnets[$i].name; mask = $yaml.subnets[$i].mask}
 }
-<#Line 18-19
+<#Line 18-21
 Here is a workaround for passing a YAML object in "New-AzResourceGroupDeployment" command. 
 When passing a regular PSObject in the command, I recieved an error because ARM wanted
 a "System.Collection.Hashtable" instead of a "System.Object"
 
-To pass mutiple hashtables in the ARM Template. I created array with mutiple hashtables
-to pass that parameter as an array instead of an object. 
+To pass mutiple hashtables in the ARM Template. I created ForLoop to append the subnet array 
+to create mutiple hashtables to pass that parameter as an array instead of an object. 
 
 In the ARM Template, I had to the change the "testObj" parameter to accept arrays instead
 of objects.
